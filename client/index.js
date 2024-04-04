5a62865ba04af034e2755b8222f585aa689db086
OkoliDaniel <okolidaniel57@gmail.com>
2024-04-04 17:48:45 +0100

    implement app

--- client/index.js
+++ client/index.js
@@ -1,3 +1,4 @@
+const { argv } = require('node:process');
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
@@ -5,12 +6,26 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // Read `name`, passed as a CLI argument from stdin.
+  const args = argv;
+  if (args.length !== 3) {
+    console.log(`Expected one argument, got ${args.length - 2} argument(s) instead.`)
+    return
+  }
+  const name = args[2];
 
+  // get the merkle proof of `name` from the nicelist merkle tree.
+  const merkleTree = new MerkleTree(niceList);
+  const nameIdx = niceList.findIndex((element) => { return name === element });
+  const proof = merkleTree.getProof(nameIdx);
+
+  // send the merkle proof to the server for verification, along with `name`.
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof,
+    name,
   });
 
+  // print the result of verification.
   console.log({ gift });
 }
 
